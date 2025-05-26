d91acf7866b4139ad72a7537884e905a8dc6c94a
Naman2004singh <naman.2singh9@gmail.com>
2025-05-26 16:32:45 +0530

    Refactor StaticContainer to use Stack for improved layout; add CircleAvatar for user profile and adjust text field positioning.

--- lib/view/home/widgets/static_container.dart
+++ lib/view/home/widgets/static_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:music_app/utils/app_colors.dart';
 import 'package:music_app/utils/app_constants.dart';
 import 'package:music_app/utils/app_strings.dart';
@@ -11,56 +12,75 @@ class StaticContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(
-          horizontal: AppConstants.mainPadding, vertical: AppConstants.size30),
-      height: AppConstants.screenHeight(context) * 0.4,
-      decoration: const BoxDecoration(
-        gradient: AppColors.homeGradient,
-        borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(AppConstants.borderRadius),
-            bottomRight: Radius.circular(AppConstants.borderRadius)),
-      ),
-      child: Column(
-        children: [
-          Row(
+    return Stack(
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(
+              horizontal: AppConstants.mainPadding,
+              vertical: AppConstants.size30),
+          height: AppConstants.screenHeight(context) * 0.4,
+          decoration: const BoxDecoration(
+            gradient: AppColors.homeGradient,
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(AppConstants.borderRadius),
+                bottomRight: Radius.circular(AppConstants.borderRadius)),
+          ),
+          child: Column(
             children: [
-              Expanded(
-                child: TextField(
-                  style: AppTextstyle.headlineMedium,
-                  decoration: AppInputDecoration.textFieldDecoration(
-                      hintText: "Search 'Punjabi Lyrics'",
-                      prefixicon: Icons.search,
-                      suffixIcon: Icons.mic_none),
-                ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      style: AppTextstyle.headlineMedium,
+                      decoration: AppInputDecoration.textFieldDecoration(
+                          hintText: "Search 'Punjabi Lyrics'",
+                          prefixicon: Icons.search,
+                          suffixIcon: Icons.mic_none),
+                    ),
+                  ),
+                  const SizedBox(width: AppConstants.size10),
+                  CircleAvatar(
+                    backgroundColor: AppColors.profileColor,
+                    child: IconButton(
+                        onPressed: () {}, icon: const Icon(Icons.person)),
+                  )
+                ],
               ),
-              const SizedBox(width: AppConstants.size10),
-              CircleAvatar(
-                backgroundColor: AppColors.profileColor,
-                child: IconButton(
-                    onPressed: () {}, icon: const Icon(Icons.person)),
-              )
+              SizedBox(
+                height: AppConstants.screenHeight(context) * 0.05,
+              ),
+              const Text(
+                AppStrings.text01,
+                style: AppTextstyle.headlineLarge,
+              ),
+              const Text(
+                AppStrings.text02,
+                style: AppTextstyle.newStyle,
+              ),
+              const Text(
+                AppStrings.text03,
+                style: AppTextstyle.headlineMedium,
+              ),
+              const SizedBox(height: AppConstants.size20),
+              const BookNowButton()
             ],
           ),
-          SizedBox(
-            height: AppConstants.screenHeight(context) * 0.05,
+        ),
+        Positioned(
+          bottom: 10.0,
+          left: -30.0,
+          child: SvgPicture.asset(
+            "assets/images/musicPlayer.svg",
           ),
-          const Text(
-            AppStrings.text01,
-            style: AppTextstyle.headlineLarge,
+        ),
+        Positioned(
+          bottom: 10.0,
+          right: -30.0,
+          child: SvgPicture.asset(
+            "assets/images/musicKeyboard.svg",
           ),
-          const Text(
-            AppStrings.text02,
-            style: AppTextstyle.newStyle,
-          ),
-          const Text(
-            AppStrings.text03,
-            style: AppTextstyle.headlineMedium,
-          ),
-          const SizedBox(height: AppConstants.size20),
-          const BookNowButton()
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
